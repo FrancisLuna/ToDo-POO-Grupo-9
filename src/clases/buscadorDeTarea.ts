b2aa8bf4b5a2910115a6e417a37d88516f12706f
SantinoLattuada <lattuadasantino@hotmail.com>
2024-11-05 17:23:12 -0300

    feat(diagrama, buscadorDeTarea, organizadorDeTarea), agrego nuevos metodos y de paso actualizo el Diagrama

--- src/clases/buscadorDeTarea.ts
+++ src/clases/buscadorDeTarea.ts
@@ -1,4 +1,7 @@
+import { Moment } from "moment";
+import Etiqueta from "./etiqueta";
 import Tarea from "./tarea";
+
 export default class BuscadorDeTarea{
     
     private tareas: Tarea[];
@@ -21,6 +24,14 @@ export default class BuscadorDeTarea{
 
     public getTareasPendientes(): Tarea[]{
         return this.tareas.filter(tarea => tarea.getEstadoActual() === ESTADO.Pendiente);
+    }
+
+    public getTareasXEtiquetas(etiqueta: Etiqueta): Tarea[]{
+        return this.tareas.filter(tarea => tarea.getEtiquetas().some(etiquetaTarea => etiquetaTarea.getNombre() === etiqueta.getNombre()))
+    }
+
+    public getTareasXFechaDeVencimiento(fechaVencimiento: Moment): Tarea[]{
+        return this.tareas.filter(tarea => tarea.getFechaVencimiento() === fechaVencimiento)
     }
 }
 
--- src/clases/organizadorDeTarea.ts
+++ src/clases/organizadorDeTarea.ts
@@ -15,4 +15,10 @@ export default class OrganizadorDeTarea{
     public ordenarTareasPorFecha(): Tarea[] {
         return this.tareas.sort((a, b) => a.getFechaCreacion().valueOf() - b.getFechaCreacion().valueOf());
     }
+
+    public ordenarTareasPorTitulo(): Tarea[]{
+        //LocaleCompare: compara dos strings, devuelve negativo si el "a" va antes del "b"
+        //o positivo en viceversa, 0 en caso de que sean iguales
+        return this.tareas.sort((a, b) => a.getTitulo().localeCompare(b.getTitulo()));
+    }
 }

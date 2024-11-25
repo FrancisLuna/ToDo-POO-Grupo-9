3f0912e7bb9faec828304d0ff1dc804eabba960a
paoladera <paolarladera@gmail.com>
2024-11-24 22:39:59 -0300

    Refactor: La clase abstracta Clasificador ahora es una interfaz.

--- src/clases/categoria.ts
+++ src/clases/categoria.ts
@@ -1,16 +1,35 @@
-import Clasificador from "./clasificador";
+import Clasificador from "../interfaces/clasificador";
 
 /**
- * Clase que extiende los atributos y métodos de la clase Clasificador y representa una categoría 
+ * Clase que implementa los métodos de la interfaz Clasificador y representa una Categoría 
  * para clasificar tareas.
  */
-export default class Categoria extends Clasificador{
+export default class Categoria implements Clasificador{
+
+    /**Nombre de la categoría.*/
+    private nombre: string;
     
     /**
      * Crea un nuevo objeto Categoría.
      * @param nombre - El nombre de la categoría.
      */
     constructor(nombre: string){
-        super(nombre);
+        this.nombre = nombre;
+    }
+
+    /**
+     * Permite asignar y actualizar el nombre a la categoría.
+     * @param nombre - El nuevo nombre a asignar.
+     */
+    public setNombre(nombre: string): void{
+        this.nombre = nombre;
+    }
+
+    /**
+     * Permite obtener el nombre de la categoría.
+     * @returns El nombre de la categoría.
+     */
+    public getNombre(): string{
+        return this.nombre;
     }
 }
--- src/clases/etiqueta.ts
+++ src/clases/etiqueta.ts
@@ -1,16 +1,35 @@
-import Clasificador from "./clasificador";
+import Clasificador from "../interfaces/clasificador";
 
 /**
- * Clase que extiende los atributos y métodos de la clase Clasificador y representa una etiqueta 
+ * Clase que implementa los métodos de la interfaz Clasificador y representa una Etiqueta 
  * para clasificar tareas.
  */
-export default class Etiqueta extends Clasificador{
+export default class Etiqueta implements Clasificador{
+    
+    /**Nombre de la etiqueta.*/
+    private nombre: string;
     
     /**
      * Crea un nuevo objeto Etiqueta.
      * @param nombre - El nombre de la etiqueta.
      */
     constructor(nombre: string){
-        super(nombre);
+        this.nombre = nombre;
+    }
+
+    /**
+     * Permite asignar y actualizar el nombre a la etiqueta.
+     * @param nombre - El nuevo nombre a asignar.
+     */
+    public setNombre(nombre: string): void{
+        this.nombre = nombre;
+    }
+
+    /**
+     * Permite obtener el nombre de la etiqueta.
+     * @returns El nombre de la etiqueta.
+     */
+    public getNombre(): string{
+        return this.nombre;
     }
 }

d818a03809df2d03e155135e258f8e8f15cc5510
Augustomcdev <augustomcdev@gmail.com>
2024-11-25 17:46:10 -0300

    merge con develop

--- tests/tarea.test.ts
+++ tests/tarea.test.ts
@@ -33,67 +33,57 @@ describe('Tarea', () =>{
         expect(tarea.getEtiquetas()).toBeNull;
     });
 
-    it('Debe asegurar modificar el id de la tarea', () => {
-        tarea.setId(10);
-        expect(tarea.getId()).toBe(10);
-    })
-
-    it('debe modificar la fecha de creación de la tarea', () => {
-        tarea.setFechaCreacion(moment('2024-11-24'));
-        expect(tarea.getFechaCreacion()).toEqual(moment('2024-11-24'));
-    })
-
-    it('debe permitir que se pueda cambiar el titulo de la tarea', () => {
+    it('Debe asegurar que se pueda cambiar el titulo de la tarea', () => {
         tarea.setTitulo('Crear diagrama de clase de la aplicación');
         expect(tarea.getTitulo()).toBe('Crear diagrama de clase de la aplicación');
     });
 
-    it('debe que se pueda asignar una descripción a la tarea', () => {
+    it('Debe asegurar que se pueda asignar una descripción a la tarea', () => {
         tarea.setDescripcion('Se debe crear el diagrama de clase y el diagrama se secuencia');
         expect(tarea.getDescripcion()).toBe('Se debe crear el diagrama de clase y el diagrama se secuencia');
     });
 
-    it('debe que se pueda acceder al historial de estados de la tarea', () => {
+    it('Debe asegurar que se pueda acceder al historial de estados de la tarea', () => {
         const historialDeEstados = tarea.getEstados();
         expect(historialDeEstados.size).toBe(1);
         expect(historialDeEstados.has(ESTADO.Pendiente)).toBe(true);
         expect(historialDeEstados.get(ESTADO.Pendiente)?.isSame(tarea.getFechaCreacion()));
     });
 
-    it('debe que se pueda cambiar el estado actual de la tarea', () => {
+    it('Debe asegurar que se pueda cambiar el estado actual de la tarea', () => {
         tarea.setEstado(ESTADO.EnProgreso);
         expect(tarea.getEstadoActual()).toBe(ESTADO.EnProgreso);
     });
 
-    it('verificar que se lance una excepción cuando la tarea ya se encuentre en el estado al que se pretende cambiar', () => {
+    it('Debe verificar que se lance una excepción cuando la tarea ya se encuentre en el estado al que se pretende cambiar', () => {
         tarea.setEstado(ESTADO.EnProgreso);
-        expect(() => tarea.setEstado(ESTADO.EnProgreso)).toThrow();
+        expect(() => tarea.setEstado(ESTADO.EnProgreso)).toThrow(EstadoInvalido);
     });
 
-    it('debe que se pueda cambiar la prioridad de la tarea', () => {
+    it('Debe asegurar que se pueda cambiar la prioridad de la tarea', () => {
         tarea.setPrioridad(PRIORIDAD.Alta);
         expect(tarea.getPrioridad()).toBe(PRIORIDAD.Alta);
     });
 
-    it('debe que se pueda actualizar el avance de la tarea', () => {
+    it('Debe asegurar que se pueda actualizar el avance de la tarea', () => {
         tarea.setAvance(AVANCE['50%']);
         expect(tarea.getAvance()).toBe(AVANCE['50%']);
     });
 
-    it('debe que se pueda cambiar la fecha de vencimiento a la tarea', () => {
+    it('Debe asegurar que se pueda cambiar la fecha de vencimiento a la tarea', () => {
         const fechaVencimiento = moment().add(7, 'days');
         tarea.setFechaVencimiento(fechaVencimiento);
         expect(tarea.getFechaVencimiento().isSame(fechaVencimiento)).toBe(true);
     });
 
-    it('debe que se pueda asignar una categoría a la tarea', () => {
+    it('Debe asegurar que se pueda asignar una categoría a la tarea', () => {
         const categoria1 = mock<Categoria>();
         categoria1.setNombre('Estudio');
         tarea.setCategoria(categoria1);
         expect(tarea.getCategoria()).toBe(categoria1);
     });
 
-    it('debe que se puedan asignar etiquetas a la tarea', () => {
+    it('Debe asegurar que se puedan asignar etiquetas a la tarea', () => {
         const etiqueta1 = mock<Etiqueta>();
         const etiqueta2 = mock<Etiqueta>();
 
@@ -110,7 +100,7 @@ describe('Tarea', () =>{
         expect(etiquetas.length).toBe(2);
     });
 
-    it('debe que se pueda eliminar una etiqueta existente de la tarea por su nombre', () => {
+    it('Debe asegurar que se pueda eliminar una etiqueta existente de la tarea por su nombre', () => {
         const etiqueta1 = mock<Etiqueta>();
         const etiqueta2 = mock<Etiqueta>();
 
